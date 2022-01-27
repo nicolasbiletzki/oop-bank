29614009ccd70085bda92ded1251706f1515d9ac
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-01-27 17:19:16 -0300

    Adicionando as Classes Banco e Conta

---
+++ oop-bank/Classes/Conta.cs
@@ -0,0 +1,58 @@
+﻿using oop_bank.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_bank.Classes
+{
+    public abstract class Conta : Banco, IConta
+    {
+        public Conta()
+        {
+            this.NumeroDaConta = "0001";
+            Conta.NumeroDaContaSequencial++;
+        }
+
+        public double Saldo { get; protected set; }
+        public string NumeroDaAgencia { get; private set; }
+        public string NumeroDaConta { get; private set; }
+
+        public static int NumeroDaContaSequencial { get; private set; }
+
+        public double ConsultarSaldo()
+        {
+            return this.Saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+        public bool Sacar(double valor)
+        {
+            if(valor > this.Saldo)
+            return false;
+
+
+            this.Saldo -= valor;
+            return true;
+            
+        }
+        public string GetCodigoDoBanco()
+        {
+            return this.CodigoDoBanco;
+        }
+
+        public string GetNumeroAgencia()
+        {
+            return this.NumeroDaAgencia;
+        }
+
+        public string GetNumeroDaConta()
+        {
+            return this.NumeroDaConta;
+        }
+
+
+    }
+}
